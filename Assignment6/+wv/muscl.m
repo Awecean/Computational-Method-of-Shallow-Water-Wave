52891886c9841aff5e39747f764d8c86b8ec4bc7
Awecean <theawecean@gmail.com>
2025-05-13 21:37:18 +0800

    Update muscl.m

--- Assignment6/+wv/muscl.m
+++ Assignment6/+wv/muscl.m
@@ -1,4 +1,6 @@
 function [up, um] = muscl(u)
+    % ouput the intercell data  with subindex i-1/2
+    % where the subindex of u is i (for cell-averaged data)
     unew = [3*u(1)-2*u(2) 2*u(1)-u(2) u 2*u(end)-u(end-1) 3*u(end)-2*u(end-1)];
 
     duplus = unew(2:end)-unew(1:end-1);
