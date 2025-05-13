8dae14e015dc045404c37334fd48703d7e8736bd
Awecean <theawecean@gmail.com>
2025-05-13 22:06:20 +0800

    Update ssprkB.m

--- Assignment6/+wv/ssprkB.m
+++ Assignment6/+wv/ssprkB.m
@@ -7,23 +7,25 @@ function [eta_next, U_next, H_next] = ssprkB(etanow, Unow, h, hp, hm, dt, dx, nL
 % we can yield the data of next moment
 % eta_next, U_next, H_next
     g = 9.81;
-    [H_temp1, H_temp2, H_next, HU_temp1, HU_temp2, HU_next] = deal(zeros(1,nL));
+    [H_temp1, H_temp2, H_next, HU_temp1, HU_temp2, HU_next] = deal(zeros(1,nL)); %創建空的陣列，大小同增加虛魅網格後的h長度
     H = etanow+h;
 
     % round 1-phase 1 impose bodary condition and the muscl, hllc
-    etanow = [etanow(4) etanow(3) etanow(3:end-2) etanow(end-2) etanow(end-3)];
-    U = [-Unow(4) -Unow(3) Unow(3:end-2) -Unow(end-2) -Unow(end-3)];
-    HU = H.*U;
-    [Um, Up] = wv.muscl(U); [etam, etap] = wv.muscl(etanow);
-    [F, G] = wv.hllc(etam, etap, Um, Up, hm, hp, h);
+    etanow = [etanow(4) etanow(3) etanow(3:end-2) etanow(end-2) etanow(end-3)]; %使用mirror boundary
+    U = [-Unow(4) -Unow(3) Unow(3:end-2) -Unow(end-2) -Unow(end-3)]; %使用mirror boundary
+    HU = H.*U; %計算當前(原先)時刻的HU (下標為i)
+    [Um, Up] = wv.muscl(U); [etam, etap] = wv.muscl(etanow); %藉由muscl重構i-1/2處的數值
+    [F, G] = wv.hllc(etam, etap, Um, Up, hm, hp, h); % 由以上數據，建立F,G 於i-1/2處的數值，長度為h長(nL)
     % round 1-phase 2 SSP-RK
     for i = 3:nL-2
-        H_temp1(i) = H(i)-dt/dx*(F(i+1)-F(i));
-        HU_temp1(i) = HU(i)-dt/dx*(G(i+1)-G(i))+dt/2/dx*g*etanow(i)*(h(i+1)-h(i-1));
+        H_temp1(i) = H(i)-dt/dx*(F(i+1)-F(i)); %計算temp1時，(ssp-rk)1/3階段後的臨時值H*_i
+        HU_temp1(i) = HU(i)-dt/dx*(G(i+1)-G(i))+dt/2/dx*g*etanow(i)*(h(i+1)-h(i-1)); %計算temp1時，(ssp-rk)1/3階段後的臨時值H*_i
     end
     %fprintf(min(abs(H_temp1)));
-    eta_temp1 = H_temp1-h;
-    U_temp1 = HU_temp1./H_temp1;
+    H_temp1 = [H_temp1(4) H_temp1(3) H_temp1(3:end-2) H_temp1(end-2) H_temp1(end-3)]; %引入boundary condition，避免HU./H時出錯
+    HU_temp1 = [HU_temp1(4) HU_temp1(3) H_temp1(3:end-2) HU_temp1(end-2) HU_temp1(end-3)]; %引入boundary condition，避免HU.H時出錯
+    eta_temp1 = H_temp1-h; %藉由[H]-h建立eta_temp1
+    U_temp1 = HU_temp1./H_temp1; %藉由[HU]./[H]
     % round 2-phase 1 
     eta_temp1 = [eta_temp1(4) eta_temp1(3) eta_temp1(3:end-2) eta_temp1(end-2) eta_temp1(end-3)];
     U_temp1 = [-U_temp1(4) -U_temp1(3) U_temp1(3:end-2) -U_temp1(end-2) -U_temp1(end-3)];
